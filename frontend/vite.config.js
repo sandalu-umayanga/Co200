38e10747833bae5eccf6b5be0fe85af940020106
sandalu umayanga <sandaluumayangaofficial@gmail.com>
2024-10-13 12:34:29 +0530

    Prepare for Heroku deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  root: '.', // Explicitly set the root directory
   plugins: [react()],
   build: {
     outDir: 'dist', // Output directory for build files
