5e684a4fe79cf157a3a441d87824e212c33ed2eb
sandalu umayanga <sandaluumayangaofficial@gmail.com>
2024-10-15 10:10:48 +0530

    frontend new

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // Specify the output directory
-    rollupOptions: {
-      input: './index.html', // Specify the entry point
-    },
-  },
-  base: './', // Specify the base URL
 })
